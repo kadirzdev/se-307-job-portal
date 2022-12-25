b6bf1230fb296f23f85851310488fd4cd5d7d8b6
eryulutas <eryulutas@gmail.com>
2022-12-26 00:32:50 +0300

    Revert "Merge branch 'main' of https://github.com/kadirzdev/se-307-job-portal"
    This reverts commit 7186fd43adcb0f1990d10857b52f3303af7a0bb0, reversing
    changes made to 3e5c774c08288595f797b31b665ac91c5315719f.

--- JobPortalSystem/JobPortalSystem/Job.cs
+++ JobPortalSystem/JobPortalSystem/Job.cs
@@ -3,16 +3,17 @@ namespace JobPortalSystem;
 // Job Portal System Jobs
 public class Job
 {
+    
     public int JobId { get; set; }
     public string JobTitle { get; set; }
     public string JobDescription { get; set; }
     public string JobLocation { get; set; }
-    public int JobExperience { get; set; }
-    public int JobSalary { get; set; }
+    public string JobExperience { get; set; }
+    public string JobSalary { get; set; }
     public string JobCompany { get; set; }
 
     
-    public Job(int jobId, string jobTitle, string jobDescription, string jobLocation, int jobExperience, int jobSalary, string jobCompany)
+    public Job(int jobId, string jobTitle, string jobDescription, string jobLocation, string jobExperience, string jobSalary, string jobCompany)
     {
         JobId = jobId;
         JobTitle = jobTitle;
@@ -22,12 +23,5 @@ public class Job
         JobSalary = jobSalary;
         JobCompany = jobCompany;
     }
-    
-    // Display Job Details in a List
-    public override string ToString()
-    {
-        return $"Job ID: {JobId} | Job Title: {JobTitle} | Job Description: {JobDescription} | Job Location: {JobLocation} | Job Experience: {JobExperience} | Job Salary: {JobSalary} | Job Company: {JobCompany}";
-    }
-
 
 }
--- JobPortalSystem/JobPortalSystem/JobSeeker.cs
+++ JobPortalSystem/JobPortalSystem/JobSeeker.cs
@@ -7,28 +7,8 @@ public class JobSeeker
     public string Email { get; set; }
     public string Phone { get; set; }
     
-    public int Experience { get; set; }
-    
     // Resume Object
+    public Resume Resume { get; set; }
 
-    // Constructor
-    public JobSeeker(string name, string email, string phone, int experience)
-    {
-        Name = name;
-        Email = email;
-        Phone = phone;
-        Experience = experience;
-    }
     
-    
-    // Resume
-    public void DisplayResume()
-    {
-        Console.WriteLine("Name: " + Name);
-        Console.WriteLine("Email: " + Email);
-        Console.WriteLine("Phone: " + Phone);
-        Console.WriteLine("Experience: " + Experience);
-    }
-
-
 }
